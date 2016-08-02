72afbcd8231bea543904aba5f27c084788680c75
Pivotal Tracker <labs-tracker-team@pivotal.io>
2016-08-02 01:32:55 -0700

    add example of fuzz generative test

--- client/app/bundles/PersistentEcho/Utils/ReverserTest.elm
+++ client/app/bundles/PersistentEcho/Utils/ReverserTest.elm
@@ -2,15 +2,23 @@ module Utils.ReverserTest exposing (..)
 
 import Test exposing (..)
 import Expect
+import Fuzz exposing (..)
+import String exposing (reverse)
 
 import Utils.Reverser exposing (reverseIt)
 
 reverser =
-  describe "Reverser"
-    [ describe "reverseIt"
+  describe "Reverser reverseIt"
+    [ describe "test"
       [ test "reverses" <|
         \() ->
           reverseIt "Missy Elliott"
             |> Expect.equal "ttoillE yssiM"
+      ]
+    , describe "fuzz"
+      [ fuzzWith { runs = 100 } ( string ) "reverses" <|
+        \ str ->
+          reverseIt str
+            |> Expect.equal (reverse str)
       ]
     ]
