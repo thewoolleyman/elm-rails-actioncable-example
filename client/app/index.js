42613f8c878b9d8894427770d8a9dc347f88c5d5
Chad Woolley <thewoolleyman@gmail.com>
2016-09-01 03:53:51 -0700

    comments and cleanup on actioncable client integration

--- client/app/index.js
+++ client/app/index.js
@@ -1,27 +1,41 @@
 'use strict';
 
-const Elm = require('./bundles/PersistentEcho/App.elm');
-const mountNode = document.getElementById('main');
-const main = Elm.PersistentEcho.App.embed(mountNode);
 const cableUrl = appSettings.websocketProtocol + '//' + appSettings.origin + '/cable';
 const actionCable = ActionCable.createConsumer(cableUrl);
+const mountNode = document.getElementById('main');
+// embed the Elm entry point module which contains the `main` function into the HTML document
+const Elm = require('./bundles/PersistentEcho/App.elm');
+const app = Elm.PersistentEcho.App.embed(mountNode);
+
+// 'StateChannel' must correspond to a Rails subclass of ApplicationCable::Channel on the server.
 const channel = actionCable.subscriptions.create('StateChannel', {
   connected() {
+    // called when websocket channel is connected - unused for now
   },
 
   disconnected() {
+    // called when websocket channel is disconnected - unused for now
   },
 
-  update(state) {
-    console.log("---> StateChannel performing update with state: " + state);
-    return this.perform('update', { state }); // shorthand for "state: state"
-  },
-
+  // `received` is the standard function ActionCable calls when `ActionCable.server.broadcast` is
+  // invoked via Rails on the server to send data to the client on this channel via websockets.
+  // i.e. the single server -> client websocket connections
   received(state) {
     console.log("<--- StateChannel received state: " + state);
-    main.ports.receiveUpdate.send(state);
+    // send data received from the server on this ActionCable websocket channel to an "incoming" Elm port/subscription
+    app.ports.receiveUpdate.send(state);
   },
 });
 
-// TODO: is there a more elegant approach than this bind?
-main.ports.publishUpdate.subscribe(channel.update.bind(channel));
+// `publishUpdate` is a custom callback function the Elm port/command calls to send data to the server via websockets.
+const publishUpdate = (state) => {
+  console.log("---> StateChannel performing update with state: " + state);
+  // send data to the server over the websocket channel
+  // 'update' must correspond to a ruby method defined on this channel's corresponding Rails subclass
+  // of ApplicationCable::Channel on the server.
+  return channel.perform('update', { state }); // shorthand for "state: state"
+};
+
+// subscribe to an "outgoing" Elm port/command on the client with an ActionCable callback function which will
+// send data to the server via websockets when it is invoked
+app.ports.publishUpdate.subscribe(publishUpdate);
