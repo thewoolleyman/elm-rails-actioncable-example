87159af17a80a9993282deb5250426ac8ad2e1fd
Chad Woolley <thewoolleyman@gmail.com>
2016-09-30 16:05:32 -0700

    add an example module in a separate file

--- client/app/bundles/PersistentEcho/Main.elm
+++ client/app/bundles/PersistentEcho/Main.elm
@@ -1,10 +1,12 @@
 port module Main exposing (..)
 
-import Html exposing (Html, Attribute, div, input, text)
+import Html exposing (Html, Attribute, div, span, input, text, a)
 import Html.App as Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 import String
+
+import Utils.Reverser exposing (reverseIt)
 
 main =
   Html.program -- change to non beginnerProgram
@@ -57,6 +59,17 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
   div []
-    [ input [ placeholder "type model state", value model.state, onInput Publish ] []
-    , div [] [ text (model.state) ]
+    [
+      div [] [
+        span [] [ text "Send the state down: " ]
+      , input [ placeholder "type model state", value model.state, onInput Publish ] []
+      ]
+    , div [] [
+        span [] [ text "Flip it and reverse it: " ]
+      , span [] [
+          a [ href "https://youtu.be/UODX_pYpVxk", target "_" ] [
+            text (reverseIt model.state)
+          ]
+        ]
+      ]
     ]
