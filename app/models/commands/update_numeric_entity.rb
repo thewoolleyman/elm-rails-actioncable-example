9ceb770422f97569b3347a65bf41b678e636eb10
Chad Woolley <thewoolleyman@gmail.com>
2016-10-09 20:54:25 -0700

    update readme to reflect support for multiple TextualEntities

--- app/models/commands/update_numeric_entity.rb
+++ app/models/commands/update_numeric_entity.rb
@@ -3,6 +3,7 @@ module Commands
     def invoke(data)
       entity = NumericEntity.find_by_id(1)
       unless entity
+        # NOTE: We don't bother with command/event support for creating a numeric entity, we assume always exactly one
         entity = NumericEntity.create(id: 1, integer: 0)
       end
       integer = data.fetch('integer')
